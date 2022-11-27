08027fc323a2b6acabd391278b60adf290064bcd
Katrina <katrina.siegfried@gmail.com>
2022-11-27 00:09:06 -0700

    Fixed sysdig wouldn't terminate

--- Live_Detection_POC/detect_root_escalation.c
+++ Live_Detection_POC/detect_root_escalation.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
 
 #define BUFFER_LENGTH 511
 
@@ -24,6 +25,7 @@ int main( int ac, char* av[] )
       int fd;
       fd = fileno(fp);
       fsync(fd);
+      fflush(stdout);
       if(!fp)
       {
          fprintf(stderr, "Error - unable to open %s\n", av[2]);
@@ -38,12 +40,23 @@ int main( int ac, char* av[] )
          {
             printf("\nAN UNAUTHORIZED ROOT ESCALATION HAS BEEN DETECTED:\n%s\n", lineBuffer);
             fclose(fp);
-            exit(-1);
+
+            printf("TERMINATING PROCESSES...\n");
+            int total_length = 512;
+            char line[total_length];
+            FILE * command = popen("pidof sysdig","r");
+            fgets(line,total_length,command);
+            pid_t pid = strtoul(line,NULL,10);
+            pclose(command);
+            printf("PID OF SYSDIG: %d\n", pid);
+            kill(pid, SIGKILL);
+            sleep(1);
+            printf("Program exiting...\n");
+            exit(0);
          }
       }
-
-      fclose(fp);
+          fclose(fp);
    }
-
    return 0;
 }
+               
