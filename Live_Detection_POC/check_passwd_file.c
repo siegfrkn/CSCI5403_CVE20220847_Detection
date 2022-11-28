c13632d389aeced502a3afe11aef7ad4613459b2
Katrina <katrina.siegfried@gmail.com>
2022-11-27 18:59:39 -0700

    Update comments

--- Live_Detection_POC/check_passwd_file.c
+++ Live_Detection_POC/check_passwd_file.c
@@ -1,3 +1,19 @@
+/*
+ * check_passwd_file.c
+ * This is a secondary file which executes the pwck command in a loop to continually
+ * monitor the /etc/passwd file to detect any changes to user manipulatons. This file is
+ * executed from the main program. If a change in the file is detected than the admin
+ * user who executed the main program will be alerted, and the sibling processes will
+ * be terminated.
+ *
+ * Compile this file with
+ * gcc check_passwd_file.c -o check_passwd_file
+ *
+ *
+ * Author Katrina Siegfried, 2022
+ *
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -7,22 +23,43 @@
 int main()
 {
    printf("\n\n[SCAN] Beginning live scan of /etc/passwd file\n\n");
-   /* local variable definition */
    int ret;
 
-   /* while loop execution */
+   /*
+    * This while loop executes infinitely every 2 second if a change to the /etc/passwd file
+    * is detected, in which case the process alerts the user, terminates the sibling
+    * processes, and exits.
+    */
    while(1) 
    {
       int printCount;
       ret = system("pwck -rq") / 255;
+      
+      /*
+       * The system call to pwck will return with 2 if a change in the /etc/passwd file
+       * is detected.
+       */
       if (ret == 2)
       {
+         /*
+          * Only print the detection message one time.
+          */
          if (printCount < 1)
          {
             printf("\n\n[SCAN] CHANGE IN PASSWORD FILE DETECTED\n");
             printCount++;
          }
+         
+         /*
+          * Wait long enough that if this detection is made immediately that all of the child
+          * processes have properly initialized. This is a TODO item for the future to use shared memory to
+          * properly synchronize the processes.
+          */
          sleep(10);
+         
+         /*
+          * Terminate the sibling processes before exiting.
+          */
          system("pkill sysdig");
          system("pkill detect_root_esc");
          printf("\n\n[SCAN] Terminating sibling processes...\n");
